e729823ef44a7b527a618f6e10cc2d755c0b0a8b
sam l <sluongo718@yahoo.com>
2020-08-27 06:25:14 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,14 +15,5 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-  if person == "Freddy Mercury"
-    contact_details_hash.each do
-      |attrubute, data|
-        if attribute == :favorite_ice_cream_flavors
-        binding.pry
-      end
-    end
-  end 
-end
+  
 end

bbfe69d67302c99f7ff2c347acd655845cb9264e
sam l <sluongo718@yahoo.com>
2020-08-27 06:22:07 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-  if person == "freddy Mercury"
+  if person == "Freddy Mercury"
     contact_details_hash.each do
       |attrubute, data|
         binding.pry
